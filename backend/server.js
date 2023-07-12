64fec2b2b788f09101421c9ce131f5abe3fc5393
Tri Tran <ttran@LABA.PSWAS.LOCAL>
2023-07-12 15:39:19 -0700

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/api/login', (req, res) => {
+app.use(express.static("/build"));
+
+app.post("/api/login", (req, res) => {
   const { username, password } = req.body;
 
-  if (username === 'admin' && password === 'password') {
+  if (username === "admin" && password === "password") {
     res.json({ success: true });
   } else {
-    res.status(401).json({ success: false, message: 'Invalid username or password' });
+    res
+      .status(401)
+      .json({ success: false, message: "Invalid username or password" });
   }
 });
 
-app.post('/api/signup', (req, res) => {
+app.post("/api/signup", (req, res) => {
   const { username, email, password } = req.body;
 
-  res.json({ success: true, message: 'Signup successful' });
+  res.json({ success: true, message: "Signup successful" });
 });
 
 app.listen(8000, () => {
-  console.log('Server is running on port 8000');
+  console.log("Server is running on port 8000");
 });
